3b5674dbb48632693488447650bc71ea71ba9b27
Johns Tsai <ax_ex@hotmail.com>
2017-06-28 19:49:50 -0700

    NYT article ajax implememted

--- js/script.js
+++ js/script.js
@@ -18,8 +18,24 @@ function loadData() {
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address +'';
     $body.append('<img class="bgimg" src="' + streetviewUrl +'">');
     // New York Times AJAX
-    $.getJSON(URL, function(data){
-        console.log(data);
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        url += '?' + $.param({
+            'api-key': "05913cc4b24c4753bf467f0344f785d6",
+            'q': cityStr,
+            'sort': "newest"
+        });
+
+    $.getJSON(url, function(data){
+        $nytHeaderElem.text('New York Time Articles About ' + cityStr);
+        articles = data.response.docs;
+        for (var i=0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">' + 
+                '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + 
+                '<p>' + article.snippet + '</p>' + '</li>');
+        };
+    }).error(function(e) {
+        $nytHeaderElem.text('New York Time Articles can not be loaded.');
     });
 
 
